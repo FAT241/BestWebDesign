63f607bbd0ef2de4f943845f2f0ddb5ac6e5d86b
NgyMinh <ngminhqb05@gmail.com>
2025-05-11 19:57:16 +0700

    commit tối ngày 11/5

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,5 +1,7 @@
 const header = document.querySelector(".header");
+const toggleBtn = document.getElementById("toggleHeaderBtn");
 
+// Xử lý khi scroll để thêm class 'scrolled'
 window.addEventListener("scroll", () => {
 	if (window.scrollY > 50) {
 		header.classList.add("scrolled");
